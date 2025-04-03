9566e98e7e585a5506699eb630812468a4b03702
SQLIGROUP\aginepro <aginepro@sqli.com>
2025-04-03 16:27:22 +0200

    update web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9f99a4ce3bda75d105ec2eb5d1315dcb
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "8fe04e3f47727d302a416fc50d89c3fc",
+"flutter_bootstrap.js": "c52ad7728d8398c4cfccb17089229df3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
